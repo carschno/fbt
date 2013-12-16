4287670a61ef70a098e302fa4dc83bd13e8ed044
Carsten Schnober <carschno@gmail.com>
2013-12-16 08:56:39 +0100

    Perform tokenization in chunks and write to files.

--- dataTokenizer.py
+++ dataTokenizer.py
@@ -4,9 +4,9 @@ import logging
 import itertools
 from time import asctime
 from collections import Counter
+import functools
 
 import joblib
-import functools
 
 from Utils import read_zip
 
@@ -22,46 +22,99 @@ sentence_splitter = PunktSentenceTokenizer()
 
 
 def tokenize(text, tagset):
+    """
+    Compute a bag of words of the given text, retaining only the tokens contained in the tagset.
+    Performs sentence splitting and tokenization.
+    @param text: A string to tokenize
+    @type text: str
+    @param tagset: A set containing the tokens to retain in result
+    @type tagset: set
+    @return: A Series mapping tokens to frequencies
+    @rtype: pd.Series
+    """
     return pd.Series(Counter(filter(lambda x: x in tagset, map(str.lower, itertools.chain.from_iterable(
         map(tokenizer.tokenize, sentence_splitter.tokenize(text)))))))
 
 
 def chunks(l, n):
+    """
+    Return a generator dividing the list l into chunks of size n.
+    @param l:
+    @param n:
+    @return:
+    """
     for i in xrange(0, len(l), n):
         yield l[i:i + n]
 
 
+def get_tagset(s, splitchar="-"):
+    """
+    Generate a tagset from the given series index while splitting tags from multiple tokens
+    @param s:
+    @param splitchar: character to detect multi-token indizes
+    @return:
+    @rtype: set
+    """
+    logger.info(asctime() + " Generating tagset from {0} indizes.".format(len(s)))
+    tagset = set(s.index)
+
+    logger.info(asctime() + " Finding multi-word expressions in tagset.")
+    mwes = set()
+    for tag in itertools.ifilter(lambda x: splitchar in x, tagset):
+        mwes.update(str.split(tag.replace(splitchar, " ")))
+    logger.info(asctime() + " Adding {0} tokens from multi-word expressions to tagset.".format(len(mwes)))
+    return tagset | mwes
+
+
 def main():
+    tokenizationsindex = dict()
+
+    logger.info("{0} Reading tags from '{1}...".format(asctime(), tagcache))
+    tags = joblib.load(tagcache)
+    data = read_zip(trainingzip, trainingfile, cols=["Id", "Title", "Body", "Tags"], index_col=0,
+                    count=nrows).drop_duplicates(cols="Title")
+    test = read_zip(testzip, testfile, cols=["Id", "Title", "Body"], count=nrows)
+
+    logger.info(asctime() + " Merging training set and test set...")
+    predictions = pd.merge(test, data, on="Title", how="left").drop_duplicates("Id")
+    print predictions
+
+    missing = predictions.index[predictions.Tags.isnull()]
+
+    logger.info(asctime() + " Tokenizing {0} entries from {1} to {2}...".format(
+        len(missing, predictions.Id[missing[0]], predictions.Id[missing[1]])))
+    tokenize_ = functools.partial(tokenize, tagset=get_tagset(tags))
+
+    for indizes in chunks(missing, 10000):
+        logger.info(asctime() + " Tokenizing {0} indizes between {1} and {2}...".format(len(indizes),
+                                                                                        predictions.Id[indizes[0]],
+                                                                                        predictions.Id[indizes[-1]]))
+        outfile = tokenizationsfile + str(indizes[0])
+        tokenizationsindex[predictions.Id[indizes[0]]] = outfile
+        tokenizations = pd.Series(index=predictions.Id[indizes], dtype="O")
+        tokenizations[predictions.Id[indizes]] = (
+            predictions.Title[indizes] + " " + predictions.Body_x[indizes].map(clean_html)).map(tokenize_)
+
+        logger.info(
+            asctime() + " Writing tokenizations for indizes between {0} and {1} to file '{2}'.".format(
+                predictions.Id[indizes[0]], predictions.Id[indizes[-1]], outfile))
+        joblib.dump(tokenizations, outfile)
+
+    logger.info(asctime() + " Writing tokenization index  to '{0}'.".format(tokenizationsindexfile))
+    joblib.dump(tokenizationsindex, tokenizationsindexfile)
+
+    logger.info(asctime() + " Done.")
+
+
+if __name__ == "__main__":
     trainingzip = "/home/carsten/facebook/Train.zip"
     trainingfile = "Train.csv"
     testzip = "/home/carsten/facebook/Test.zip"
     testfile = "Test.csv"
-    outfile = "/home/carsten/facebook/cache/tokenizations"
-
+    tokenizationsfile = "/home/carsten/facebook/cache/tokenizations"
+    tokenizationsindexfile = "/home/carsten/facebook/cache/tokenizationsindex"
     tagcache = "/home/carsten/facebook/cache/tags"
-    logger.info("{0} Reading tags from '{1}...".format(asctime(), tagcache))
-    tags = joblib.load(tagcache)
 
-    data = read_zip(trainingzip, trainingfile, cols=["Id", "Title", "Body", "Tags"], index_col=0,
-                          count=nrows).drop_duplicates(cols="Title")
-    test = read_zip(testzip, testfile, cols=["Id", "Title", "Body"], count=nrows)
-
-    predictions = pd.merge(test, data, on=["Title", "Body"], how="left").drop_duplicates("Id")
-
-    #predictions = pd.merge(test, data, on="Title", how="left", left_index=True, right_index=True)
-    missing = predictions.index[predictions.Tags.isnull()]
-
-    logger.info(asctime() + " Tokenizing {0} entries...".format(len(missing)))
-    tokenize_ = functools.partial(tokenize, tagset=set(tags.index))
-
-    tokenizations = pd.Series(index=predictions.Id[missing], dtype="O")
-    tokenizations[predictions.Id[missing]] = (predictions.Title[missing] + " " + predictions.Body[missing].map(clean_html)).map(tokenize_)
-
-    logger.info(asctime() + " Writing to file '{0}'".format(outfile))
-    joblib.dump(tokenizations, outfile)
-    logger.info(asctime()+ " Done.")
-
-if __name__ == "__main__":
-    nrows = None
+    nrows = 100000
 
     main()
--- token_counter.py
+++ token_counter.py
@@ -1,33 +1,29 @@
 import pandas as pd
 import csv
 import logging
-from nltk import clean_html
 from time import asctime
-from collections import Counter
 import joblib
 
 from Utils import read_zip
-import dataTokenizer
 
 __author__ = 'carsten'
-
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 logger.addHandler(logging.StreamHandler())
 
 
-__author__ = 'carsten'
-
 def find_tags(token_series, tags, mwes=list(), max_tags=5):
     """
 
-    @param text: a text
+    @param token_series: a series representing a document's bag of words
+    @type token_series: pd.Series
     @param mwes: a list of multi-word (multi-token) expressions to be searched in the text
     @type mwes: list
-    @param mwe_separator: separator used in MWEs ("visual-studio-2010")
     @param max_tags: maximum number or tags assigned to text
-    @return:
+    @type max_tags: 5
+    @return: a list of tags assigned to the document
+    @rtype: list
     """
     #tokens = dataTokenizer.tokenize(text, tags.index)
     #token_series = pd.Series(Counter(tokens))   # TODO: normalize token and tag frequencies
@@ -71,35 +67,34 @@ def main():
     logger.info(asctime() + " Reading tag counts from '{0}'...".format(tagcache))
     tags = joblib.load(tagcache, mmap_mode="r")
 
-    logger.info(asctime() + " Loading tokenizations from '{0}'...".format(tokenizationfile))
-    tokenizations = joblib.load(tokenizationfile, mmap_mode="r")
+    logger.info(asctime() + " Loading tokenizations index from '{0}'...".format(tokenizationsindexfile))
+    tokenizationindex = joblib.load(tokenizationsindexfile)
 
     test = read_zip(testzip, testfile, cols=["Id", "Title", "Body"], count=nrows)
 
     logger.info(asctime() + " Merging training data and test data...")
     predictions = pd.merge(test, data, on="Title", how="left").drop_duplicates("Id")
-    #predictions = pd.merge(test, data, on="Title", how="left", left_index=True, right_index=True)
 
     missing = predictions.index[predictions.Tags.isnull()]
-    logger.info("{0} Computing {1} missing tags using tokens in title and in body.".format(asctime(), len(missing)))
+    logger.info("{0} Computing {1} missing tags between {2} and {3}...".format(asctime(), len(missing),
+                                                                               predictions.Id[missing[0]],
+                                                                               predictions.Id[missing[-1]]))
 
     #predictions[missing] = (predictions.Title[missing].map(str.lower) + " " + predictions.Body[missing].map(clean_html).map(str.lower)).map(find_tags)
 
     #predictions[missing] = (predictions.Title[missing] + " " + predictions[missing].map(clean_html)).map(tokenize)
-    counter = 0
+
+    tokenizations = pd.Series()
     for i in missing:
-        counter += 1
-        if counter % 1000 == 0:
-            logging.info("{0} {1} of {2} done.".format(asctime(), counter, len(missing)))
         #predictions.Tags[i] = " ".join(find_tags(predictions.Title[i] + " " + clean_html(predictions.Body[i])))
         #print tokenizations[predictions.Id[i]]
-        if predictions.Id[i] in tokenizations.index:
-            predictions.Tags[i] = " ".join(find_tags(tokenizations[predictions.Id[i]], tags))
-        else:
-            logger.warn(asctime() + " No tokenization found for {0}.".format(predictions.Id[i]))
+        if predictions.Id[i] not in tokenizations.index:
+            logger.info(asctime() + " Loading indizes for {0} from '{1}'".format(predictions.Id[i],
+                                                                                 tokenizationindex[predictions.Id[i]]))
+            tokenizations = joblib.load(tokenizationindex[predictions.Id[i]])
+        predictions.Tags[i] = " ".join(find_tags(tokenizations[predictions.Id[i]], tags))
 
     outfile = "/home/carsten/facebook/predictions_{0}documents.csv".format(nrows)
-
 
     logger.info(asctime() + " Writing predictions to '{0}'...".format(outfile))
     predictions.to_csv(outfile, index=False, cols=["Id", "Tags"], quoting=csv.QUOTE_ALL)
@@ -114,9 +109,9 @@ if __name__ == "__main__":
 
     columns = ["Id", "Title", "Tags"]
     tagcache = "/home/carsten/facebook/cache/tags"
-    tokenizationfile = "/home/carsten/facebook/cache/tokenizations"
-
-    nrows = 10000
+    #tokenizationfile = "/home/carsten/facebook/cache/tokenizations"
+    tokenizationsindexfile = "/home/carsten/facebook/cache/tokenizationsindex"
+    nrows = 100000
 
     main()
 
